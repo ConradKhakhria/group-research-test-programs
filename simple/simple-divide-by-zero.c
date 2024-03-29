f13aebf78648cd1fcf058fa272ef2018c7eda3b8
Conrad Khakhria <conradjkhakhria@gmail.com>
2024-03-29 15:14:39 +0000

    made it even simpler

--- simple/simple-divide-by-zero.c
+++ simple/simple-divide-by-zero.c
@@ -2,8 +2,5 @@
 
 
 int main() {
-    float divisors[5] = {2.0, 4.0, 3.0, 1.0, 0.0};
-
-    for (int i = 0; i < 5; i++)
-        printf("%d\n", 15.0 / divisors[i]);
+    printf("%d\n", 15.0 / 0.0);
 }
